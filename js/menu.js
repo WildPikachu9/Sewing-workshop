0045134df2bdf536a8e0aa5bbbaba58e5619c5c3
YarynaHuk <jasmin.huk@gmail.com>
2023-09-11 19:21:22 +0200

    added Smooth Scrolling for site

--- js/menu.js
+++ js/menu.js
@@ -6,3 +6,21 @@ burger.addEventListener('click', function () {
 		activeElement[i].classList.toggle('show');
 	}
 });
+
+
+$(document).ready(function(){
+	$('.hash').on('click', function(e){
+		if(this.hash !== ""){
+			e.preventDefault();
+
+			let hash = this.hash;
+			console.log(hash);
+
+			$('html, body').animate({
+				scrollTop: $(hash).offset().top
+			}, 800, function(){
+				window.location.hash = hash;
+			});
+		}
+	})
+})
